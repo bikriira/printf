34924f94e0d26ade90bc8d8cb24220f8d2d5e0d5
bikriira <iradufashabikri@gmail.com>
2023-11-14 21:59:31 -0800

    made my code to well return the number of chars printed by switching to malloc, so that i can free after _printf call

--- 0-printf.c
+++ 0-printf.c
@@ -11,47 +11,55 @@
 int _printf(const char *format, ...)
 {
     int i = 0;
-    static int counter = 0;
+    int *counter = malloc(sizeof(int));
+	int counter_helper;
     va_list args;
 	
 	if (format == NULL)
 		return (-1);
+	*counter = 0;
     va_start(args, format);
     while (format[i] != '\0')
     {
         if (format[i] == '%')
         {
             i++;
-            if (format[i] == 's' || format[i] == 'c')
+            if (format[i] == 's' || format[i] == 'c'|| format[i] == 'd')
             {
                 if (format[i] == 's')
                 {
                     char *str = va_arg(args, char *);
-                    str_handler(str, &counter);
+                    str_handler(str, counter);
                 }
                 else if (format[i] == 'c')
                 {
-                    _putchar(va_arg(args, int), &counter);
+                    _putchar(va_arg(args, int), counter);
                 }
+				else if (format[i] == 'd')
+				{
+					print_digit(va_arg(args, int), 10, counter);
+				}
             }
             else if (format[i] == '%')
             {
-                _putchar('%', &counter);
+                _putchar('%', counter);
             }
 			else
 			{
-				_putchar('%', &counter);
-				_putchar(format[i], &counter);
+				_putchar('%', counter);
+				_putchar(format[i], counter);
 			}
         }
         else
         {
-            _putchar(format[i], &counter);
+            _putchar(format[i], counter);
         }
         i++;
     }
     va_end(args);
-    return (counter);
+	counter_helper = *counter;
+	free(counter);
+    return (counter_helper);
 }
 
 /**
@@ -71,3 +79,38 @@ void str_handler(char *str, int *counter)
 		str_indexer++;
 	}
 }
+
+/**
+*/
+
+void print_digit(int num, int base, int *counter)
+{
+	char *symbols = "0123456789abcdef";
+
+	if (num < 0)
+	{
+		_putchar('-', counter);
+		print_digit(-(long)num, base, counter);
+	}
+	else if (num < base)
+	{
+		_putchar(symbols[num], counter);
+	}
+	else if (num > base)
+	{
+		int helper1 = num / base;
+		int helper2 = num % base;
+	
+		print_digit(helper1, base, counter);
+		print_digit(helper2, base, counter);
+	}
+}
+
+
+
+
+
+
+
+
+
--- main.h
+++ main.h
@@ -4,5 +4,6 @@
 int _printf(const char *format, ...);
 int _putchar(char c, int *counter_addr);
 void str_handler(char *str, int *counter);
+void print_digit(int num, int base, int *counter);
 
 #endif
